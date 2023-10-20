ec24cc96a73c7b8d6425db398be222a0c52ff69d
Fundxx <fundiexx@gmail.com>
2023-10-21 00:58:55 +0300

    lottie init

--- client/src/pages/FourZeroFour.jsx
+++ client/src/pages/FourZeroFour.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import fourZeroFour from '../assets/lottie/four_zero_four.json'
 
 const FourZeroFour = () => {
     return (
         <div>
             BBY RAGE I CANT FIND THIS PAGE
+            <Lottie animationData={fourZeroFour}/>
         </div>
     );
 };
