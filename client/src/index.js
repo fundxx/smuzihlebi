3074a4d1d6f544030dfcb2e9ff2bd254c39e5f15
TrojanDll <dzafarovt36@gmail.com>
2023-10-20 20:51:59 +0300

    feat: something

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import store from "../store/store";
 import App from "./App";
+import { ThemeProvider } from "@mui/material";
+import theme from "./Theme";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+        </ThemeProvider>
     </React.StrictMode>
 );
