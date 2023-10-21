2e35ce4d71751636d13f1a9be34d8fb7c11cc533
TrojanDll <dzafarovt36@gmail.com>
2023-10-21 07:49:20 +0300

    feat: login added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Account from "./pages/Account";
 import TournamentsList from "./pages/TournamentsList";
 import Layout from "./routes/Layout";
 import FourZeroFour from "./pages/FourZeroFour";
+import LogIn from "./pages/LogIn";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Route path="/" element={<Layout />}>
                 <Route path="/account" element={<Account />} />
                 <Route path="/tournament" element={<TournamentsList />} />
+                <Route path="/login" element={<LogIn />} />
                 <Route path="/tournament/:id" element={<div>About</div>} />
                 <Route
                     path="/tournament/:id/table"
--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./style/style.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
